d5b4d9ae93be3becb5e4cd9b49312f504f4b2714
Dan BROOKS <githubcom@domaindesign.ca>
2013-08-21 11:36:13 -0700

    VanArts week 12

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+	  combine: {
+		files: {
+		  'public/css/main.min.css': ['assets/css/demo.css', 'assets/css/two.css']
+		}
+	  }
+	},
+	markdown: {
+		all: {
+		  files: [
+			{
+			  expand: true,
+			  src: '*.md',
+			  dest: 'public/html/',
+			  ext: '.html'
+			}
+		  ]
+		}
+	  }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-markdown');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin', 'markdown']);
+
+};
